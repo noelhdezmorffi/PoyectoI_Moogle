efef73b8b8e79a30643c2a1322b0a3aa952f20a7
noelhdezmorffi <128205759+noelhdezmorffi@users.noreply.github.com>
2023-05-20 21:23:26 -0400

    Update SearchResult.cs

--- MoogleEngine/SearchResult.cs
+++ MoogleEngine/SearchResult.cs
@@ -14,6 +14,30 @@ public class SearchResult
         this.Suggestion = suggestion;
     }
 
+    public static void Inicializar(SearchItem[] items,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            items[i] = new SearchItem(" ", " ", 0);
+        }
+    }
+
+    public static void OrdenaPorScore(SearchItem[] items)
+    {
+        for (int k = 1; k < items.Length; k++)
+        {
+            for (int i = 0; i < items.Length - 1; i++)
+            {
+                SearchItem temp = items[i];
+                if (items[i].Score < items[i + 1].Score)
+                {
+                    items[i] = items[i + 1];
+                    items[i + 1] = temp;
+                }
+            }
+        }
+    }
+
     public SearchResult() : this(new SearchItem[0]) {
 
     }
